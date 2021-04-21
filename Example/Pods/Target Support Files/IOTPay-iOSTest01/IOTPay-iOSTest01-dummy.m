05c4dc59df40452937df497de52734d54a36b6b4
Dan-YY <dan@dansstudio.ca>
2021-04-21 11:32:57 -0700

    just a msg

---
+++ Example/Pods/Target Support Files/IOTPay-iOSTest01/IOTPay-iOSTest01-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_IOTPay_iOSTest01 : NSObject
+@end
+@implementation PodsDummy_IOTPay_iOSTest01
+@end
---
+++ Example/Pods/Target Support Files/Pods-IOTPay-iOSTest01_Example/Pods-IOTPay-iOSTest01_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IOTPay_iOSTest01_Example : NSObject
+@end
+@implementation PodsDummy_Pods_IOTPay_iOSTest01_Example
+@end
